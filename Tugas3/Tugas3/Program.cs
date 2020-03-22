1210e39e779ad2171068bb6104d5b3e8c4780aaf
faisalmohamad <61940650+faisalmohamad@users.noreply.github.com>
2020-03-22 18:02:04 +0700

    Add files via upload

---
+++ Tugas3/Tugas3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Tugas3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi1 = new Taxi();
+            Taxi taxi2 = new Taxi();
+            Taxi taxi3 = new Taxi();
+
+            taxi1.driver ="rifki";
+            taxi1.onduty = "no";
+            taxi1.number = "4";
+
+            taxi2.driver = "bondan";
+            taxi2.onduty = "no";
+            taxi2.number = "2";
+
+            taxi3.driver = "yanto";
+            taxi3.onduty = "y/n";
+            taxi3.number = "6";
+
+            taxi1.taxiinfo();
+            taxi1.pickuppassenger();
+
+
+            taxi2.taxiinfo();
+            taxi2.dropofpassenger();
+
+            taxi3.taxiinfo();
+            taxi3.pickuppassenger();
+            taxi3.dropofpassenger();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tugas3/Tugas3/Taxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas3
+{
+    class Taxi
+    {
+        public string driver { get; set; }
+        public string onduty { get; set; }
+        public string number { get; set; }
+
+        public void taxiinfo()
+        {
+            Console.WriteLine("Driver Name  : {0}", driver);
+            Console.WriteLine("Onduty       : {0}", onduty);
+            Console.WriteLine("Numpassenger : {0}", number);
+            Console.WriteLine("__________________________");
+        }
+        public void pickuppassenger()
+        {
+            Console.WriteLine("Sedang menjemput penumpang\n", driver);
+        }
+        public void dropofpassenger()
+        {
+            Console.WriteLine("Selesai mengantar penumpang\n");
+        }
+
+    }
+}
